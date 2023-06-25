d6d8e25b02066abcc58318f92630b072da2bbedd
davvd <davvd@zold.io>
2023-06-26 01:05:43 +0300

    #36 gem configured

--- lib/zold/score.rb
+++ lib/zold/score.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2018-2019 Yegor Bugayenko
+# Copyright (c) 2018-2023 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -36,7 +36,7 @@ require 'time'
 # {White Paper}[https://papers.zold.io/wp.pdf].
 #
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2018-2019 Yegor Bugayenko
+# Copyright:: Copyright (c) 2018-2023 Yegor Bugayenko
 # License:: MIT
 module Zold
   # Score
