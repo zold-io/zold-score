8cd5966fd6cb2ced7c604e27171a7ed5be09aa34
davvd <davvd@zold.io>
2025-01-23 12:19:09 +0300

    year set to 2025

--- lib/zold/score.rb
+++ lib/zold/score.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2018-2023 Zerocracy
+# Copyright (c) 2018-2025 Zerocracy
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -36,7 +36,7 @@ require 'time'
 # {White Paper}[https://papers.zold.io/wp.pdf].
 #
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2018-2023 Zerocracy
+# Copyright:: Copyright (c) 2018-2025 Zerocracy
 # License:: MIT
 module Zold
   # Score
